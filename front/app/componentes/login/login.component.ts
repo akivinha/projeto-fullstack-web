9b3157be1ab5b2b549793af393e4c2ec3f5d6faf
GregoryVinicius <gregoryviniciuscla@gmail.com>
2022-09-22 09:03:58 -0300

    foi

---
+++ front/app/componentes/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'registro',
+    templateUrl: './registro.Component.html'
+})
+
+export class RegistroComponent implements OnInit {
+
+    public title: string;
+
+    constructor() {
+        this.title = 'Cadastre-se';
+    }
+
+    ngOnInit(): void {
+        console.log('Componente carregado...');
+    }
+}
